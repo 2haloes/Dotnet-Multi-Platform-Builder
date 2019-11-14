00bccbc689132e46ec6651345255fd83e9d7fc54
2haloes <rhysh@live.co.uk>
2019-11-14 08:30:34 +0000

    Setting up and trying to download OS information

---
+++ Dotnet-Multi-Platform-Builder/Dotnet-Multi-Platform-Builder/Classes/WebFunctions.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Dotnet_Multi_Platform_Builder.Classes
+{
+    class WebFunctions
+    {
+        public HttpClient webClient { get; set; }
+
+        // Should be set up the least number of times possible
+        public WebFunctions()
+        {
+            webClient = new HttpClient();
+        }
+
+        public dynamic DownloadJsonOSList()
+        {
+            HttpResponseMessage response = webClient.GetAsync("https://raw.githubusercontent.com/dotnet/corefx/master/src/pkg/Microsoft.NETCore.Platforms/runtime.json").Result;
+            Console.WriteLine("Gotten response!");
+            try
+            {
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            Console.WriteLine("Response good!");
+
+            return JsonConvert.DeserializeObject<object>(response.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
--- Dotnet-Multi-Platform-Builder/Dotnet-Multi-Platform-Builder/Program.cs
+++ Dotnet-Multi-Platform-Builder/Dotnet-Multi-Platform-Builder/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dotnet_Multi_Platform_Builder.Classes;
+using System;
 
 namespace Dotnet_Multi_Platform_Builder
 {
@@ -6,7 +7,25 @@ namespace Dotnet_Multi_Platform_Builder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            dynamic OSinfo;
+
+
+            Console.WriteLine("Downloading JSON...");
+            WebFunctions webHelper = new WebFunctions();
+
+            try
+            {
+                OSinfo = webHelper.DownloadJsonOSList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Download failed");
+            }
+            
+
+            Console.WriteLine("Downloaded successfully!");
+
+            Console.ReadKey();
         }
     }
 }
